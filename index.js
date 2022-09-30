924ca77eedaa6381fd086fdb03fe65fe5920d420
Nikita Piskarev <npiskarev23@gmail.com>
2022-09-30 15:44:22 +0300

    Fix factorial -0 commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Создать рекурсивную функцию вычисления факториала числа.Факториал - произведение всех натуральных чисел от 1 до x.
 // 5! = 1 * 2 * 3 * 4 * 5 = 120
 function factorial(num) {
+  if (num < 1) {
+    return false;
+  }
   if (num <= 1) {
     return 1;
   } else {
@@ -9,5 +12,8 @@ function factorial(num) {
 }
 
 function factorial(num) {
+  if (num < 1) {
+    return false;
+  }
   return num <= 1 ? 1 : num * factorial(num - 1);
 }
